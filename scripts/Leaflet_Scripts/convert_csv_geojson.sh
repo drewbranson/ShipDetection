d49f6e0076f8cdca17c4f81c5f87aad8a5230dcc
Drew Branson <37157743+drewbranson@users.noreply.github.com>
2022-08-19 11:00:46 -0700

    gernalizationwork

--- scripts/Leaflet_Scripts/convert_csv_geojson.sh
+++ scripts/Leaflet_Scripts/convert_csv_geojson.sh
@@ -1,5 +1,7 @@
-csv2geojson ../output/ShipDetections.csv > ../output/geodata.geojson
+AOI="$1"
 
-sed -i '1 s/^.*$/var json_ShipDetections_1 = {/' ../output/geodata.geojson
+csv2geojson ../output/ShipDetections_${AOI}.csv > ../output/geodata.geojson
 
-cp ../output/geodata.geojson ../../ShipDetection.github.io/data/ShipDetections_1.js
+sed -i '1 s/^.*$/var json_ShipDetections_'${AOI}'_1 = {/' ../output/geodata.geojson
+
+cp ../output/geodata.geojson ../../ShipDetection.github.io/data/ShipDetections_${AOI}_1.js
--- scripts/Runall_shpDetection.sh
+++ scripts/Runall_shpDetection.sh
@@ -1,6 +1,7 @@
 ### Runall_shpDetection.sh
 ## call with
-# Runall_shpDetection.sh "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" "/mnt/sdb1/Products/BlackSea_ShipDet" BlackSea_buffer200
+# Runall_shpDetection.sh "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" "/mnt/sdb1/Products/BlackSea_ShipDet" BlackSea
+# Runall_shpDetection.sh "/mnt/sdb1/Data/MediterraneanSea_East/toprocess" "/mnt/sdb1/Products/MediterraneanSea_East" MediterraneanSea_East
 
 sourceDirectory="$1"
 targetDirectory="$2"
@@ -12,16 +13,16 @@ cd /home/drew/Documents/GitHub/ShipDetection/scripts
 python3 /home/drew/Documents/GitHub/ShipDetection/scripts/asf_download.py ${sourceDirectory} /home/drew/Documents/GitHub/ShipDetection/inputs/SeaMask/${AOI}.shp ${AOI}
 # python3 /home/drew/Documents/GitHub/ShipDetection/scripts/asf_download.py "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" /home/drew/Documents/GitHub/ShipDetection/inputs/SeaMask/BlackSea_buffer200.shp BlackSea_buffer200
 
-# # ./processDataset.sh ShipDetection.xml SHP_DET.propterties "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" "/mnt/sdb1/Products/BlackSea_ShipDet" SHP
-# sed "s/\AOI/$AOI/g" ShipDetection.xml > ShipDetection_tmp.xml
-# ./processDataset.sh ShipDetection_tmp.xml SHP_DET.propterties ${sourceDirectory} ${targetDirectory} SHP 
+# ./processDataset.sh ShipDetection.xml SHP_DET.propterties "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" "/mnt/sdb1/Products/BlackSea_ShipDet" SHP
+sed "s/\AOI/$AOI/g" ShipDetection.xml > ShipDetection_tmp.xml
+./processDataset.sh ShipDetection_tmp.xml SHP_DET.propterties ${sourceDirectory} ${targetDirectory} SHP 
 
-# python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py ${targetDirectory} ${AOI}
+python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py ${targetDirectory} ${AOI}
 # # python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py "/mnt/sdb1/Products/BlackSea_ShipDet" BlackSea_buffer200
 
 
-# ./Leaflet_Scripts/convert_csv_geojson.sh 
+./Leaflet_Scripts/convert_csv_geojson.sh ${AOI}
 
 # ./cleaning.sh
 
-# ./../../gitpush_shpDet_web.sh
+./../../gitpush_shpDet_web.sh
