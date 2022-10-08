b5da0b4d0a16489df48ba70e3b82f4f4a48a33ef
drewbranson <37157743+drewbranson@users.noreply.github.com>
2022-10-08 16:58:20 -0700

    tmop

--- scripts/Runall_shpDetection.sh
+++ scripts/Runall_shpDetection.sh
@@ -2,6 +2,7 @@
 ## call with
 # Runall_shpDetection.sh "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" "/mnt/sdb1/Products/BlackSea_ShipDet" BlackSea
 # Runall_shpDetection.sh "/mnt/sdb1/Data/MediterraneanSea_East/toprocess" "/mnt/sdb1/Products/MediterraneanSea_East" MediterraneanSea_East
+# Runall_shpDetection.sh "/home/drew/Documents/Data/BlackSea/toprocess" "/home/drew/Documents/Products/BlackSea" BlackSea
 
 sourceDirectory="$1"
 targetDirectory="$2"
@@ -13,16 +14,16 @@ cd /home/drew/Documents/GitHub/ShipDetection/scripts
 python3 /home/drew/Documents/GitHub/ShipDetection/scripts/asf_download.py ${sourceDirectory} /home/drew/Documents/GitHub/ShipDetection/inputs/SeaMask/${AOI}.shp ${AOI}
 # python3 /home/drew/Documents/GitHub/ShipDetection/scripts/asf_download.py "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" /home/drew/Documents/GitHub/ShipDetection/inputs/SeaMask/BlackSea.shp BlackSea
 
-# ./processDataset.sh ShipDetection.xml SHP_DET.propterties "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" "/mnt/sdb1/Products/BlackSea_ShipDet" SHP
-sed "s/\AOI/$AOI/g" ShipDetection.xml > ShipDetection_tmp.xml
-./processDataset.sh ShipDetection_tmp.xml SHP_DET.propterties ${sourceDirectory} ${targetDirectory} SHP 
+# # ./processDataset.sh ShipDetection.xml SHP_DET.propterties "/mnt/sdb1/Data/Ukraine/BlackSea/toprocess" "/mnt/sdb1/Products/BlackSea_ShipDet" SHP
+# sed "s/\AOI/$AOI/g" ShipDetection.xml > ShipDetection_tmp.xml
+# ./processDataset.sh ShipDetection_tmp.xml SHP_DET.propterties ${sourceDirectory} ${targetDirectory} SHP 
 
-python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py ${targetDirectory} ${AOI}
-# # python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py "/mnt/sdb1/Products/BlackSea_ShipDet" BlackSea
-# # python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py "/mnt/sdb1/Products/Mediterranean_East" Mediterranean_East
+# python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py ${targetDirectory} ${AOI}
+# # # python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py "/mnt/sdb1/Products/BlackSea_ShipDet" BlackSea
+# # # python3 /home/drew/Documents/GitHub/ShipDetection/scripts/extract_results.py "/mnt/sdb1/Products/Mediterranean_East" Mediterranean_East
 
-./Leaflet_Scripts/convert_csv_geojson.sh ${AOI}
+# ./Leaflet_Scripts/convert_csv_geojson.sh ${AOI}
 
-./cleaning.sh
+# ./cleaning.sh
 
-./../../gitpush_shpDet_web.sh
+# ./../../gitpush_shpDet_web.sh
