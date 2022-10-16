f533f87a9cefc4b108759857d7e4309bb9e339fb
Drew Branson <37157743+drewbranson@users.noreply.github.com>
2022-10-15 17:00:53 -0700

    data

--- scripts/process_everything_BM.sh
+++ scripts/process_everything_BM.sh
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/BlackSea/toprocess" "/home/drew/Documents/Products/BlackSea" "BlackSea"
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/NortheastAtlantic/toprocess" "/home/drew/Documents/Products/NortheastAtlantic" "NortheastAtlantic"
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/NorthIndian/toprocess" "/home/drew/Documents/Products/NorthIndian" "NorthIndian"
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/NorthPacific/toprocess" "/home/drew/Documents/Products/NorthPacific" "NorthPacific"
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/SouthAtlantic/toprocess" "/home/drew/Documents/Products/SouthAtlantic" "SouthAtlantic"
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/SouthEastAsia/toprocess" "/home/drew/Documents/Products/SouthEastAsia" "SouthEastAsia"
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/SouthIndian/toprocess" "/home/drew/Documents/Products/SouthIndian" "SouthIndian"
-=======
-/home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/ArabianSea/toprocess" "/home/drew/Documents/Products/ArabianSea" "ArabianSea"
-
 /home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/ArabianSea/toprocess" "/home/drew/Documents/Products/ArabianSea" "ArabianSea"
 /home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/BalticSea/toprocess" "/home/drew/Documents/Products/BalticSea" "BalticSea"
 /home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/Caribbean/toprocess" "/home/drew/Documents/Products/Caribbean" "Caribbean"
@@ -16,4 +5,4 @@
 /home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/WesternMed/toprocess" "/home/drew/Documents/Products/WesternMed" "WesternMed"
 /home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/ArabianSea/toprocess" "/home/drew/Documents/Products/ArabianSea" "ArabianSea"
 /home/drew/Documents/GitHub/ShipDetection/scripts/Runall_shpDetection.sh "/home/drew/Documents/Data/MediterraneanSeaEast/toprocess" "/home/drew/Documents/Products/MediterraneanSeaEast" "MediterraneanSeaEast"
->>>>>>> Stashed changes
+
