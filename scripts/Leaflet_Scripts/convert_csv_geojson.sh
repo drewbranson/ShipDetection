6900d55acc26f26d3ff3c75ba3066e25d13ed16c
drewbranson <37157743+drewbranson@users.noreply.github.com>
2022-11-20 13:43:12 -0800

    changed output path for the geojsons

--- scripts/Leaflet_Scripts/convert_csv_geojson.sh
+++ scripts/Leaflet_Scripts/convert_csv_geojson.sh
@@ -1,3 +1,5 @@
+# Basically just bloat after migrating to vector tiles but json-to-csv.pl will need to be modified without this
+
 AOI="$1"
 
 case $AOI in 
@@ -21,4 +23,7 @@ csv2geojson ../output/ShipDetections_${AOI}.csv > ../output/geodata.geojson
 
 sed -i '1 s/^.*$/var json_'${AOI}'_'${number}' = {/' ../output/geodata.geojson
 
-cp ../output/geodata.geojson ../../ShipDetection.github.io/data/${AOI}_${number}.js
+# for geojson 
+cp ../output/geodata.geojson ../../ShipDetection.github.io/points/data/${AOI}_${number}.js
+# for tiles 
+# cp ../output/geodata.geojson ../output/data/${AOI}_${number}.js
